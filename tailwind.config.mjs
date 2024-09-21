6889e3eeef3011dd7b130c97aec714a3271d1734
Sebastian <ssalasg.mm@gmail.com>
2024-09-21 02:00:25 -0300

    alo

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Playpen Sans"', 'sans-serif'],
+				roboto: ['Roboto', 'sans-serif'],
+			},
+		},
+	},
+
+	plugins: [],
+}
