317b00b52e451b44978f0350fde558d1ed73dcb3
Sebastian <ssalasg.mm@gmail.com>
2024-11-02 00:18:53 -0300

    xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://sneakere.github.io/MujerValiente/',
-  base: '/MujerValiente/',
+  site: 'http://www.mujeresvalientesalvolante.cl/',
 });
